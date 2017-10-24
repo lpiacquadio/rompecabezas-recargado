d8a543d7ef21212c53db82edc54055f5b91a588b
Lautaro Piacquadio <lautaropiacquadio.lp@gmail.com>
2017-10-23 21:43:27 -0300

    object Juego

--- js/juego.js
+++ js/juego.js
@@ -1,163 +1,122 @@
-var movimientos = [];
-var resolviendo = false;
-
-function resolver() {
-    resolviendo = true;
-    movimientos = movimientos.reverse();
-    movimientos.forEach(movimiento => {
-        moverEnDireccion(movimiento);
-    })
-    resolviendo = false;
-    movimientos = [];
-}
-
-// Representación de la grilla. Cada nro representa a una pieza.
-// El 9 es la posición vacía
-var grilla = [
-    [
-        1, 2, 3
-    ],
-    [
-        4, 5, 6
-    ],
-    [
-        7, 8, 9
-    ]
-];
-
-// Ac&aacute; vamos a ir guardando la posición vacía
-var posicionVacia = {
-    fila: 2,
-    columna: 2
-};
-
-// Esta función va a chequear si el Rompecabezas est&aacute; en la posición ganadora
-function chequearSiGano() {
-    var item = 0;
-    for (var fila = 0; fila < grilla.length; fila++) {
-        for (var columna = 0; columna < grilla[fila].length; columna++) {
-            item++
-            if (grilla[fila][columna] !== item) {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-// la hacen los alumnos, pueden mostrar el cartel como prefieran. Pero es importante que usen
-// esta función
-function mostrarCartelGanador() {
-    alert('Ganaste!');
-    iniciar();
-}
-
-// Intercambia posiciones grilla y en el DOM
-function intercambiarPosiciones(fila1, columna1, fila2, columna2) {
-    // Intercambio la posicion en la grilla
-    var posicionActual = grilla[fila1][columna1];
-    var posicionACambiar = grilla[fila2][columna2];
-    grilla[fila1][columna1] = posicionACambiar;
-    grilla[fila2][columna2] = posicionActual;
-
-    // Intercambio la posicion en el DOM
-    var piezaActual = document.getElementById('pieza' + posicionActual);
-    var piezaACambiar = document.getElementById('pieza' + posicionACambiar);
-    var padre =  piezaActual.parentNode;
-    padre.replaceChild(piezaActual.cloneNode(true), piezaACambiar);
-    padre.replaceChild(piezaACambiar.cloneNode(true), piezaActual);
-}
-
-// Actualiza la posición de la pieza vacía
-function actualizarPosicionVacia(nuevaFila, nuevaColumna) {
-    posicionVacia = {
-        fila: nuevaFila,
-        columna: nuevaColumna
+var Juego = function(cantidadDePiezasPorLado) {
+    this.cantidadDePiezasPorLado = cantidadDePiezasPorLado;
+    this.posicionVacia = {
+        fila: this.cantidadDePiezasPorLado - 1,
+        columna: this.cantidadDePiezasPorLado - 1
     };
-}
+    this.grilla = [];
 
-// Para chequear si la posicón está dentro de la grilla.
-function posicionValida(fila, columna) {
-    return fila >= 0 && columna >= 0 && fila < 3 && columna < 3;
-}
-
-// Movimiento de fichas, en este caso la que se mueve es la blanca intercambiando
-// su posición con otro elemento
-function moverEnDireccion(direccion) {
-
-    var nuevaFilaPiezaVacia;
-    var nuevaColumnaPiezaVacia;
-
-    if (direccion == 40) {
-        nuevaFilaPiezaVacia = posicionVacia.fila + 1;
-        nuevaColumnaPiezaVacia = posicionVacia.columna; // Intercambia pieza blanca con la pieza que está abajo suyo
-    } else if (direccion == 38) {
-        nuevaFilaPiezaVacia = posicionVacia.fila - 1;
-        nuevaColumnaPiezaVacia = posicionVacia.columna; // Intercambia pieza blanca con la pieza que está arriba suyo
-    } else if (direccion == 39) {
-        nuevaFilaPiezaVacia = posicionVacia.fila;
-        nuevaColumnaPiezaVacia = posicionVacia.columna + 1; // Intercambia pieza blanca con la pieza que está a su der
-    } else if (direccion == 37) {
-        nuevaFilaPiezaVacia = posicionVacia.fila;
-        nuevaColumnaPiezaVacia = posicionVacia.columna - 1; // Intercambia pieza blanca con la pieza que está a su izq
-    }
-
-    // Se chequea si la nueva posición es válida, si lo es, se intercambia
-    if (posicionValida(nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia)) {
-        if (!resolviendo) {
-            if (direccion == 40) {
-                movimientos.push(38);
-            }
-            if (direccion == 38) {
-                movimientos.push(40);
-            }
-            if (direccion == 39) {
-                movimientos.push(37);
-            }
-            if (direccion == 37) {
-                movimientos.push(39);
+    Juego.prototype.crearGrilla = function() {
+        var grilla = new Array(this.cantidadDePiezasPorLado);
+        var count = 1;
+        for (var fila = 0; fila < this.cantidadDePiezasPorLado; fila++) {
+            grilla[fila] = new Array(this.cantidadDePiezasPorLado);
+            for (var columna = 0; columna < grilla[fila].length; columna++) {
+                grilla[fila][columna] = count;
+                count++;
             }
         }
-        intercambiarPosiciones(posicionVacia.fila, posicionVacia.columna, nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia);
-        actualizarPosicionVacia(nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia);
+        this.grilla = grilla;
     }
 
-}
-
-// Extras, ya vienen dadas
-
-function mezclarPiezas(veces) {
-    if (veces <= 0) {
-        return;
+    Juego.prototype.posicionValida = function(fila, columna) {
+        return fila >= 0 && columna >= 0 && fila < this.cantidadDePiezasPorLado && columna < this.cantidadDePiezasPorLado;
     }
-    var direcciones = [40, 38, 39, 37];
-    var direccion = direcciones[Math.floor(Math.random() * direcciones.length)];
-    moverEnDireccion(direccion);
 
-    setTimeout(function() {
-        mezclarPiezas(veces - 1);
-    }, 100);
+    Juego.prototype.chequearSiGano = function() {
+        var count = 0;
+        for (var fila = 0; fila < this.grilla.length; fila++) {
+            for (var columna = 0; columna < this.grilla[fila].length; columna++) {
+                count++
+                if (this.grilla[fila][columna] !== count) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    Juego.prototype.mostrarCartelGanador = function() {
+        alert('Ganaste!');
+        this.iniciar();
+    }
+
+    Juego.prototype.intercambiarPosiciones = function(fila1, columna1, fila2, columna2) {
+        var posicionActual = this.grilla[fila1][columna1];
+        var posicionACambiar = this.grilla[fila2][columna2];
+        this.grilla[fila1][columna1] = posicionACambiar;
+        this.grilla[fila2][columna2] = posicionActual;
+
+        var piezaActual = document.getElementById('pieza' + posicionActual);
+        var piezaACambiar = document.getElementById('pieza' + posicionACambiar);
+        var padre =  piezaActual.parentNode;
+        padre.replaceChild(piezaActual.cloneNode(true), piezaACambiar);
+        padre.replaceChild(piezaACambiar.cloneNode(true), piezaActual);
+    }
+
+    Juego.prototype.actualizarPosicionVacia = function(nuevaFila, nuevaColumna) {
+        this.posicionVacia = {
+            fila: nuevaFila,
+            columna: nuevaColumna
+        };
+    }
+
+    Juego.prototype.moverEnDireccion = function(direccion) {
+        var nuevaFilaPiezaVacia;
+        var nuevaColumnaPiezaVacia;
+
+        if (direccion == 40) {
+            nuevaFilaPiezaVacia = this.posicionVacia.fila + 1;
+            nuevaColumnaPiezaVacia = this.posicionVacia.columna;
+        } else if (direccion == 38) {
+            nuevaFilaPiezaVacia = this.posicionVacia.fila - 1;
+            nuevaColumnaPiezaVacia = this.posicionVacia.columna;
+        } else if (direccion == 39) {
+            nuevaFilaPiezaVacia = this.posicionVacia.fila;
+            nuevaColumnaPiezaVacia = this.posicionVacia.columna + 1;
+        } else if (direccion == 37) {
+            nuevaFilaPiezaVacia = this.posicionVacia.fila;
+            nuevaColumnaPiezaVacia = this.posicionVacia.columna - 1;
+        }
+
+        if (this.posicionValida(nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia)) {
+            this.intercambiarPosiciones(this.posicionVacia.fila, this.posicionVacia.columna, nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia);
+            this.actualizarPosicionVacia(nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia);
+        }
+    }
+
+    Juego.prototype.mezclarPiezas = function(veces) {
+        if (veces <= 0) {
+            return;
+        }
+        for (var vez = 0; vez < veces; vez++) {
+            var direcciones = [40, 38, 39, 37];
+            var direccion = direcciones[Math.floor(Math.random() * direcciones.length)];
+            this.moverEnDireccion(direccion);
+        }
+    }
+
+    Juego.prototype.iniciar = function() {
+        this.mezclarPiezas(60);
+    }   
 }
 
-function capturarTeclas() {
+function capturarTeclas(juego) {
     document.body.onkeydown = (function(evento) {
         if (evento.which == 40 || evento.which == 38 || evento.which == 39 || evento.which == 37) {
-            moverEnDireccion(evento.which);
-
-            var gano = chequearSiGano();
+            juego.moverEnDireccion(evento.which);
+            var gano = juego.chequearSiGano();
             if (gano) {
                 setTimeout(function() {
-                    mostrarCartelGanador();
+                    juego.mostrarCartelGanador();
                 }, 500);
             }
             evento.preventDefault();
         }
-    })
+    });
 }
 
-function iniciar() {
-    mezclarPiezas(60);
-    capturarTeclas();
-}
-
-iniciar();
+var juego = new Juego(3);
+juego.crearGrilla();
+juego.iniciar();
+capturarTeclas(juego);
--- js/test/tests.js
+++ js/test/tests.js
@@ -5,10 +5,10 @@ describe('Creación', function() {
 
 describe('Juego', function() {
     it('El Objeto Juego está definido', function(done) {
-      if (!window.Juego){
+      if (!window.juego){
         done(err);
       }
-      else{ 
+      else{
         done();
       }
     });
@@ -17,11 +17,11 @@ describe('Juego', function() {
 describe('Tamaño de la grilla', function() {
     it('La grilla tiene el tamaño correcto', function() {
       //se crea la grilla con un valor de cantidad de piezas por lado
-      Juego.cantidadDePiezasPorLado = 5;
-      Juego.crearGrilla();
+      juego.cantidadDePiezasPorLado = 5;
+      juego.crearGrilla();
       //se evalua si el tamaño de la grilla creada es correcto
-      expect(Juego.grilla.length).to.equal(Juego.cantidadDePiezasPorLado);
-      expect(Juego.grilla[0].length).to.equal(Juego.cantidadDePiezasPorLado);
+      expect(juego.grilla.length).to.equal(juego.cantidadDePiezasPorLado);
+      expect(juego.grilla[0].length).to.equal(juego.cantidadDePiezasPorLado);
     });
   });
 });
